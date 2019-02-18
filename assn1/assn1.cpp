2c5ca236a92fedb30fddb3d25e8ee1667e3ef572
zlindsey824 <lyricist824@gmail.com>
2019-02-18 18:16:22 +0000

    Color added and switches smoothly for both lines and curves. May or may not return for color picker. Probably not though because I have no idea how to do it.

--- assn1/assn1.cpp
+++ assn1/assn1.cpp
@@ -12,6 +12,7 @@
 #include <GL/glu.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 #include <math.h>
 #include "line.h"
 #include "curve.h"
@@ -21,6 +22,13 @@
 #define WIDTH 500
 #define HEIGHT 500
 
+typedef struct{
+	double r;
+	double g;
+	double b;
+} Color;
+
+Color *colorA, *colorB;
 Line line;
 Curve curve;
 bool linesActive = false;
@@ -42,11 +50,11 @@ void init_window()
 
 /***************************************************************************/
 
-void write_pixel(int x, int y, double intensity)
+void write_pixel(int x, int y, double redInc, double greenInc, double blueInc)
                                          /* Turn on the pixel found at x,y */
 {
-
-        glColor3f (intensity, intensity, intensity);
+//printf("%f %f %f\n", redInc, greenInc, blueInc);
+        glColor3f (colorA->r + redInc, colorA->g + greenInc, colorA->b + blueInc);
         glBegin(GL_POINTS);
            glVertex3i( x, y, 0);
         glEnd();
@@ -58,19 +66,31 @@ void dda(){
   float x = line.getPoint(0).x;
   float y = line.getPoint(0).y;
   float slope = line.getSlope();
+  
+  
+  double rDiff = colorB->r - colorA->r;
+  double gDiff = colorB->g - colorA->g;
+  double bDiff = colorB->b - colorA->b;
+  
   if (fabs(slope) < 1){
+  	int xDiff = line.getPoint(1).x-line.getPoint(0).x;
+  	int xInc = 0;
     while (line.haveAllPoints() && x != line.getPoint(1).x){
-      write_pixel((int)x, (int)round(y),1.0);
+      write_pixel((int)x, (int)round(y),(rDiff/xDiff)*xInc,(gDiff/xDiff)*xInc,(bDiff/xDiff)*xInc);
       (line.getXdiff() < 0) ? x-- : x++;
       (line.getYdiff() < 0) ? y-=fabs(slope) : y+=fabs(slope);
+      xInc++;
     }
   }
   if (fabs(slope) >= 1){
     slope = line.getInvSlope();
+    int yDiff = line.getPoint(1).y-line.getPoint(0).y;
+    int yInc = 0;
     while (line.haveAllPoints() && y != line.getPoint(1).y){
-      write_pixel((int)round(x), (int)y,1.0);
+      write_pixel((int)round(x), (int)y,(rDiff/yDiff)*yInc,(gDiff/yDiff)*yInc,(bDiff/yDiff)*yInc);
       (line.getXdiff() < 0) ? x-=fabs(slope) : x+=fabs(slope);
       (line.getYdiff() < 0) ? y-- : y++;
+      yInc++;
     }
   }
 }
@@ -86,9 +106,23 @@ void display ( void )   // Create The Display Function
     y_last = -1;
     erase = false;
   }
+  colorA = new Color{0,0,0};
 
-  write_pixel(x_last,y_last,1.0);//<-you can get rid of this call if you like
+  write_pixel(x_last,y_last,1.0,1.0,1.0);//<-you can get rid of this call if you like
+  delete colorA;
   // CALL YOUR CODE HERE
+  double r,g,b;
+  int colorDivisions = 100;
+  r = (rand() % colorDivisions +1)/(double)colorDivisions;
+  g = (rand() % colorDivisions +1)/(double)colorDivisions;
+  b = (rand() % colorDivisions +1)/(double)colorDivisions;
+  colorA = new Color{r,g,b};
+  
+  r = (rand() % colorDivisions +1)/(double)colorDivisions;
+  g = (rand() % colorDivisions +1)/(double)colorDivisions;
+  b = (rand() % colorDivisions +1)/(double)colorDivisions;
+  colorB = new Color{r,g,b};
+  
   if (linesActive && line.haveAllPoints()) {
     dda();
     line.clearPoints();
@@ -96,13 +130,24 @@ void display ( void )   // Create The Display Function
 	}
   if (curvesActive && curve.haveAllPoints()){
     float divisions = 25;
+		  
+		double rDiff = colorB->r - colorA->r;
+		double gDiff = colorB->g - colorA->g;
+		double bDiff = colorB->b - colorA->b;
+		Color startColor = {colorA->r,colorA->g,colorA->b};
     for(int n = 1; n <= divisions; n++){
-      Point firstPoint = curve.getPoint((n-1)/divisions);
+			float u = (n-1)/divisions;
+      Point firstPoint = curve.getPoint(u);
       line.addPoint(firstPoint.x, firstPoint.y);
-      Point secondPoint = curve.getPoint(n/divisions);
+      colorA = new Color{startColor.r + (rDiff * u), startColor.g + (gDiff * u), startColor.b + (bDiff * u)};
+      u = (n)/divisions;
+      Point secondPoint = curve.getPoint(u);
       line.addPoint(secondPoint.x, secondPoint.y);
+      colorB = new Color{startColor.r + (rDiff * u), startColor.g + (gDiff * u), startColor.b + (bDiff * u)};
       dda();
       line.clearPoints();
+    	delete colorA;
+    	delete colorB;
     }
     curve.clearPoints();
     printf("\n");
@@ -176,6 +221,7 @@ int main (int argc, char *argv[])
    loop until the end of the data is reached.  Then the window can be closed
    using the escape key.						  */
 
+	srand(time(NULL));
 	glutInit            ( &argc, argv );
        	glutInitDisplayMode ( GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH );
 	glutInitWindowSize  ( 500,500 );
